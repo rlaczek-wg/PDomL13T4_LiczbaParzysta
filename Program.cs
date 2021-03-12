1cb5d17166dc2f0ee7d387bfe4e885ba4afce18b
Rafal Laczek <rafal_laczek@yahoo.no>
2021-03-12 11:45:37 +0100

    Add Program file

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace PracaDomowaL13T4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+     
+            while(true)
+            {
+                try
+                {
+                    Console.WriteLine("Podaj liczbe");
+                    var myNumber = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Podana przez ciebie liczba to " + myNumber);
+                    if (myNumber % 2 == 0)
+                    {
+                        Console.WriteLine("Jest to liczba parzysta");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Jest liczba nieparzysta");
+
+                    }
+
+                }
+                catch
+                {
+                    Console.WriteLine("Podana wartosc nie jest prawdopodobnie liczba :(");
+
+                }
+               
+
+            } 
+
+        }
+
+
+    }
+}
